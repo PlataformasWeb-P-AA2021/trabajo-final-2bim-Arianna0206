0e9a9b905a4aa8fc8c84ebe9156124554a05209d
Arianna Ramon <bananafish@outlook.es>
2021-07-27 15:31:29 -0500

    Subiendo cambios

---
+++ lenguaje-programacion/ProyectoFinal/src/main/java/paquete1/PlanPostPagoMinutos.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author macbookair
+ */
+public class PlanPostPagoMinutos extends PlanCelular implements Serializable {
+
+    private double minutosNacionales;
+    private double costoMinutoNacional;
+    private double minutosInternacionales;
+    private double costoMinutoInternacional;
+
+    public PlanPostPagoMinutos(Persona c, String mC, String moC, int nC,
+            double mN, double cN, double mI, double cI) {
+        super(c, mC, moC, nC);
+        minutosNacionales = mN;
+        costoMinutoNacional = cN;
+        minutosInternacionales = mI;
+        costoMinutoInternacional = cI;
+    }
+
+    public void establecerMinutosNacionales(double n) {
+        minutosNacionales = n;
+    }
+
+    public void establecerCostoMinutoNacional(double n) {
+        costoMinutoNacional = n;
+    }
+
+    public void establecerMinutosInternacionales(double n) {
+        minutosInternacionales = n;
+    }
+
+    public void establecerCostoMinutoInternacional(double n) {
+        costoMinutoInternacional = n;
+    }
+
+    public void calcularPagoMensual() {
+        pagoMensual = ((minutosNacionales * costoMinutoNacional)
+                + (minutosInternacionales * costoMinutoInternacional));
+    }
+
+    public double obtenerMinutosNacionales() {
+        return minutosNacionales;
+    }
+
+    public double obtenerCostoMinutoNacional() {
+        return costoMinutoNacional;
+    }
+
+    public double obtenerMinutosInternacionales() {
+        return minutosInternacionales;
+    }
+
+    public double obtenerCostoMinutoInternacional() {
+        return costoMinutoInternacional;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("%s\nMinutos nacionales: %.2f\n"
+                + "Costo por minutos nacionales: %.2f\n"
+                + "Minutos internacionales: %.2f\n"
+                + "Costo por minutos internacionales: %.2f\n"
+                + "Pago Mensual: %.2f\n",
+                super.toString(),
+                minutosNacionales,
+                costoMinutoNacional,
+                minutosInternacionales,
+                costoMinutoInternacional,
+                pagoMensual);
+        return cadena;
+    }
+}
+
